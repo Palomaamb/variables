a14d90955d153433877ca69679b6970fc99c1fba
Palomaamb <143645002+Palomaamb@users.noreply.github.com>
2024-02-02 10:22:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const PopRock= "🎵 Pop Rock"
+const Rock = "🎸 Rock"
+const HardRock = "🤘 Hard Rock"
+const Clásica = "🎼 Clásica"
+
+const grupo1 = {
+    nombre: "The Beatles",
+    añodefundación: "1960",
+    activo: true,
+    géneromusical: PopRock,
+};
+
+const grupo2 = {
+    nombre: "Queen",
+    añodefundación: "1970",
+    activo: false,
+    géneromusical: Rock,
+};
+
+const grupo3 = {
+    nombre: "AC DC",
+    añodefundación: "1973",
+    activo: true,
+    géneromusical: HardRock,
+};
+
+const grupo4 = {
+    nombre: "Ludwig van Beethoven",
+    añodefundación: "1770",
+    activo: false,
+    géneromusical: Clásica,
+};
+
+const grupo5 = {
+    nombre: "The Rolling Stones",
+    añodefundación: "1962",
+    activo: true,
+    géneromusical: Rock,
+};
+
+console.log(grupo1);
+console.log(grupo2);
+console.log(grupo3);
+console.log(grupo4);
+console.log(grupo5);
+
+console.log(nombre); "color:black; font-size:18px; font-weight:bold; background-color:green";
